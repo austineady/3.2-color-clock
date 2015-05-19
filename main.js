1fda690b0191143c9b57cab183bc4699bb64e57e
Austin <austineady@github.com>
2015-05-19 18:18:37 -0400

    hover stuff

--- main.js
+++ main.js
@@ -2,13 +2,18 @@
 
 var $display = document.querySelector('.time');
 var $color = document.querySelector('.colorclock');
+var date = new Date();
+var hours = checkTime(date.getHours());
+var minutes = checkTime(date.getMinutes());
+var seconds = checkTime(date.getSeconds());
+var timeArray = [hours, minutes, seconds];
+var timeDisplay = timeArray.join(':');
 
 function checkTime(i) {
   return(i < 10) ? "0" + i : i;
 }
 
-
-function displayCurrentTime() {
+function determineCurrentTime() {
   var date = new Date();
   var hours = checkTime(date.getHours());
   var minutes = checkTime(date.getMinutes());
@@ -16,7 +21,6 @@ function displayCurrentTime() {
   var timeArray = [hours, minutes, seconds];
   var timeDisplay = timeArray.join(':');
   var hexDisplay = Number(timeArray.join(''));
-  $display.textContent = timeDisplay;
   var barLength = (seconds/60)*100;
   var percentChange = String(barLength) + "%";
   var hexString = hexDisplay.toString(16);
@@ -24,9 +28,23 @@ function displayCurrentTime() {
   var colorOne = hexFormat;
   document.getElementById('line').style.width = percentChange;
   var dom = document.body;
-  dom.style.backgroundImage = '-webkit-gradient(radial, 50% 50%, 0, 50% 50%,1000, from(#' + colorOne + '), to(#353F3E))';
+  dom.style.backgroundImage = '-webkit-gradient(radial, 50% 50%, 0, 50% 50%,1200, from(#' + colorOne + '), to(#353F3E))';
+  $display.textContent = timeDisplay;
 }
 
+function displayCurrentTime() {
+  $display.textContent = timeDisplay;
+}
 
+function displayHexValue() {
+  var hexDisplay = Number(timeArray.join(''));
+  var hexString = hexDisplay.toString(16);
+  var hexFormat = ("0" + hexString).slice(-6);
+  var colorOne = hexFormat;
+  $display.textContent = colorOne;
+}
 
-window.setInterval(displayCurrentTime, 1000);
+$display.addEventListener('mouseenter', displayHexValue);
+$display.addEventListener('mouseout', displayCurrentTime);
+
+window.setInterval(determineCurrentTime, 1000);
