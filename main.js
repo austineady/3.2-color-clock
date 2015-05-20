61f4bb9cd25458431a19e78e3f232bc032b61956
Austin <austineady@github.com>
2015-05-20 11:47:23 -0400

    broke into functions, broke program

--- main.js
+++ main.js
@@ -2,15 +2,21 @@
 
 var $display = document.querySelector('.time');
 var $color = document.querySelector('.colorclock');
-var date = new Date();
-var hours = checkTime(date.getHours());
-var minutes = checkTime(date.getMinutes());
-var seconds = checkTime(date.getSeconds());
-var timeArray = [hours, minutes, seconds];
-var timeDisplay = timeArray.join(':');
+var hover = false;
 
 function checkTime(i) {
   return(i < 10) ? "0" + i : i;
+}
+
+function displayCurrentTime(timeDisplay, colorOne) {
+  barTick();
+  $display.textContent = timeDisplay;
+  var dom = document.body;
+  dom.style.backgroundImage = '-webkit-gradient(radial, 50% 50%, 0, 50% 50%,1200, from(#' + colorOne + '), to(#353F3E))';
+}
+
+function displayHexValue(colorOne) {
+  $display.textContent = colorOne;
 }
 
 function determineCurrentTime() {
@@ -20,31 +26,37 @@ function determineCurrentTime() {
   var seconds = checkTime(date.getSeconds());
   var timeArray = [hours, minutes, seconds];
   var timeDisplay = timeArray.join(':');
-  var hexDisplay = Number(timeArray.join(''));
+  displayCurrentTime(timeArray, timeDisplay);
+  determineHexValue(timeArray);
+  barTick(seconds);
+}
+
+function barTick(seconds) {
   var barLength = (seconds/60)*100;
   var percentChange = String(barLength) + "%";
-  var hexString = hexDisplay.toString(16);
-  var hexFormat = ("0" + hexString).slice(-6);
-  var colorOne = hexFormat;
   document.getElementById('line').style.width = percentChange;
-  var dom = document.body;
-  dom.style.backgroundImage = '-webkit-gradient(radial, 50% 50%, 0, 50% 50%,1200, from(#' + colorOne + '), to(#353F3E))';
-  $display.textContent = timeDisplay;
+
 }
 
-function displayCurrentTime() {
-  $display.textContent = timeDisplay;
-}
-
-function displayHexValue() {
+function determineHexValue(timeArray) {
   var hexDisplay = Number(timeArray.join(''));
   var hexString = hexDisplay.toString(16);
   var hexFormat = ("0" + hexString).slice(-6);
   var colorOne = hexFormat;
-  $display.textContent = colorOne;
+  displayHexValue(colorOne);
+  displayCurrentTime(colorOne);
 }
+  // if (hover = true) {
+  //   displayHexValue();
+  // } else {
+  //   displayCurrentTime();
+  // }
 
-$display.addEventListener('mouseenter', displayHexValue);
-$display.addEventListener('mouseout', displayCurrentTime);
 
-window.setInterval(determineCurrentTime, 1000);
+// $display.addEventListener('mouseenter', displayHexValue);
+// $display.addEventListener('mouseout', displayCurrentTime);
+
+window.setInterval(displayCurrentTime, 1000);
+displayCurrentTime();
+window.setInterval(determineHexValue, 1000);
+determineHexValue();
